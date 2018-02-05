16b16ced6f46881e480395421659efa788800a79
carldanley <carldanley@gmail.com>
2018-02-05 10:44:40 -0500

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-# source the base scripts
+# source the exports first
 . ~/dotfiles/base/.exports
-. ~/dotfiles/base/.imports
-. ~/dotfiles/base/.aliases
-. ~/dotfiles/base/.functions
 
-# register the global gitignore commands
-git config --global core.excludesfile ~/dotfiles/.gitignore
-
-# source the custom scripts
 if [ -f ~/dotfiles/custom/.exports ] ; then
   . ~/dotfiles/custom/.exports
 fi
+
+# source the imports next
+. ~/dotfiles/base/.imports
+
 if [ -f ~/dotfiles/custom/.imports ] ; then
   . ~/dotfiles/custom/.imports
 fi
+
+# source the aliases next
+. ~/dotfiles/base/.aliases
+
 if [ -f ~/dotfiles/custom/.aliases ] ; then
   . ~/dotfiles/custom/.aliases
 fi
+
+# source the functions last
+. ~/dotfiles/base/.functions
+
 if [ -f ~/dotfiles/custom/.functions ] ; then
   . ~/dotfiles/custom/.functions
 fi
 
+# register the global gitignore commands
+git config --global core.excludesfile ~/dotfiles/.gitignore
+
 # make sure nvm directory exists
 if [ ! -d ~/.nvm ] ; then
   mkdir ~/.nvm
-fi
-
-# make sure the go directory exists
-if [ ! -d ~/.go/src/github.com/carldanley ] ; then
-  mkdir -p ~/.go/src/github.com/carldanley
 fi
